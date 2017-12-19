5c3ccb64c599ed6e7973e9c538a46d87a819f8dd
Louise <louise709xu@yahoo.com>
2017-12-19 10:58:01 -0800

    fixed formatting

--- src/main.c
+++ src/main.c
@@ -187,20 +187,20 @@ void MAX_Init()
 {
     /* ----- Begin configuration register setup ----- */
 
-	/***************************************************************************
-	 * TOF1 Register - basic operating parameters for TOF measurements
-	 * TOF1[15:8]   Pulse Launcher Size
-	 * TOF1[7:4]    Pulse Launch Divider
-	 * TOF1[3]      Stop Polarity
-	 * TOF1[2]      Reserved (No effect)
-	 * TOF1[1:0]    Bias Charge Time
-	 **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF1;
-	spi_tx_config_buffer[1] = 0x0C;
-	spi_tx_config_buffer[2] = 0x10;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    /***************************************************************************
+     * TOF1 Register - basic operating parameters for TOF measurements
+     * TOF1[15:8]   Pulse Launcher Size
+     * TOF1[7:4]    Pulse Launch Divider
+     * TOF1[3]      Stop Polarity
+     * TOF1[2]      Reserved (No effect)
+     * TOF1[1:0]    Bias Charge Time
+     **************************************************************************/
+    spi_tx_config_buffer[0] = WRITE_TOF1;
+    spi_tx_config_buffer[1] = 0x0C;
+    spi_tx_config_buffer[2] = 0x10;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
-	/***************************************************************************
+    /***************************************************************************
      * TOF2 Register - details of how TOF will be measured
      * TOF2[15:13]  Stop Hits
      * TOF2[12:7]   T2 Wave Selection
@@ -208,22 +208,22 @@ void MAX_Init()
      * TOF2[3]      Reserved (No effect)
      * TOF2[2:0]    Timeout
      **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF2;
-	spi_tx_config_buffer[1] = 0xE1;
-	spi_tx_config_buffer[2] = 0x00;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    spi_tx_config_buffer[0] = WRITE_TOF2;
+    spi_tx_config_buffer[1] = 0xE1;
+    spi_tx_config_buffer[2] = 0x00;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
-	/***************************************************************************
-	 * TOF3 Register - select which waves will be used in time measurements
-	 * TOF3[15:14]  Reserved
-	 * TOF3[13:8]   HIT1 Wave Select
-	 * TOF3[7:6]    Reserved
-	 * TOF3[5:0]    HIT2 Wave Select
-	 **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF3;
-	spi_tx_config_buffer[1] = 0x05;
-	spi_tx_config_buffer[2] = 0x06;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    /***************************************************************************
+     * TOF3 Register - select which waves will be used in time measurements
+     * TOF3[15:14]  Reserved
+     * TOF3[13:8]   HIT1 Wave Select
+     * TOF3[7:6]    Reserved
+     * TOF3[5:0]    HIT2 Wave Select
+     **************************************************************************/
+    spi_tx_config_buffer[0] = WRITE_TOF3;
+    spi_tx_config_buffer[1] = 0x05;
+    spi_tx_config_buffer[2] = 0x06;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
     /***************************************************************************
      * TOF4 Register - select which waves will be used in time measurements
@@ -232,52 +232,52 @@ void MAX_Init()
      * TOF4[7:6]    Reserved
      * TOF4[5:0]    HIT4 Wave Select
      **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF4;
-	spi_tx_config_buffer[1] = 0x07;
-	spi_tx_config_buffer[2] = 0x08;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    spi_tx_config_buffer[0] = WRITE_TOF4;
+    spi_tx_config_buffer[1] = 0x07;
+    spi_tx_config_buffer[2] = 0x08;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
-	/***************************************************************************
-	 * TOF5 Register - select which waves will be used in time measurements
-	 * TOF5[15:14]  Reserved
-	 * TOF5[13:8]   HIT5 Wave Select
-	 * TOF5[7:6]    Reserved
-	 * TOF5[5:0]    HIT6 Wave Select
-	 **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF5;
-	spi_tx_config_buffer[1] = 0x09;
-	spi_tx_config_buffer[2] = 0x0A;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    /***************************************************************************
+     * TOF5 Register - select which waves will be used in time measurements
+     * TOF5[15:14]  Reserved
+     * TOF5[13:8]   HIT5 Wave Select
+     * TOF5[7:6]    Reserved
+     * TOF5[5:0]    HIT6 Wave Select
+     **************************************************************************/
+    spi_tx_config_buffer[0] = WRITE_TOF5;
+    spi_tx_config_buffer[1] = 0x09;
+    spi_tx_config_buffer[2] = 0x0A;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
-	/***************************************************************************
-	 * TOF6 Register - comparator upstream
-	 * TOF6[15:8]   Comparator Return Offset Upstream
-	 * TOF6[7:0]    Comparator Offset Upstream
-	 **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF6;
-	spi_tx_config_buffer[1] = 0x23;
-	spi_tx_config_buffer[2] = 0x0A;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    /***************************************************************************
+     * TOF6 Register - comparator upstream
+     * TOF6[15:8]   Comparator Return Offset Upstream
+     * TOF6[7:0]    Comparator Offset Upstream
+     **************************************************************************/
+    spi_tx_config_buffer[0] = WRITE_TOF6;
+    spi_tx_config_buffer[1] = 0x23;
+    spi_tx_config_buffer[2] = 0x0A;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
     /***************************************************************************
      * TOF7 Register - comparator downstream
      * TOF7[15:8]   Comparator Return Offset Downstream
      * TOF7[7:0]    Comparator Offset Downstream
      **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF7;
-	spi_tx_config_buffer[1] = 0x23;
-	spi_tx_config_buffer[2] = 0x0A;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    spi_tx_config_buffer[0] = WRITE_TOF7;
+    spi_tx_config_buffer[1] = 0x23;
+    spi_tx_config_buffer[2] = 0x0A;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
     /***************************************************************************
      * TOF Measurement Delay - delay between start of pulse launch and receiver
      *                         enable
      * DLY[15:8]    Delay
      **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_TOF_MEAS_DELAY;
-	spi_tx_config_buffer[1] = 0x00;
-	spi_tx_config_buffer[2] = 0xC8;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    spi_tx_config_buffer[0] = WRITE_TOF_MEAS_DELAY;
+    spi_tx_config_buffer[1] = 0x00;
+    spi_tx_config_buffer[2] = 0xC8;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
     /***************************************************************************
      * Calibration and Control Register - calibration settings
@@ -288,15 +288,15 @@ void MAX_Init()
      * CLBRT[4:2]   Preamble Temperature Cycle
      * CLBRT[1:0]   Port Cycle Time
      **************************************************************************/
-	spi_tx_config_buffer[0] = WRITE_CLBRT_CTRL;
-	spi_tx_config_buffer[1] = 0x0E;
-	spi_tx_config_buffer[2] = 0xFF;
-	MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
+    spi_tx_config_buffer[0] = WRITE_CLBRT_CTRL;
+    spi_tx_config_buffer[1] = 0x0E;
+    spi_tx_config_buffer[2] = 0xFF;
+    MAX_SPI_TX_Config(&spi_tx_config_buffer[0]);
 
 
-	/* ----- End configuration register setup ----- */
+    /* ----- End configuration register setup ----- */
 
-	spi_tx_buffer[0] = TX_CONFIG_FLASH;       // Transfer Configuration to Flash Command
+    spi_tx_buffer[0] = TX_CONFIG_FLASH;       // Transfer Configuration to Flash Command
     MAX_SPI_TX(&spi_tx_buffer[0]);
 
     spi_tx_buffer[0] = INITIALIZE;            // Initialize
@@ -313,11 +313,11 @@ void MAX_Init()
 void SPI_Init() {
 
     SPIDRV_Init_t initData = {                                                        \
-    		  USART1,                       /* USART port                       */    \
+              USART1,                       /* USART port                       */    \
               _USART_ROUTE_LOCATION_LOC1,   /* USART pins location number       */    \
               1000000,                      /* Bitrate                          */    \
               8,                            /* Frame length                     */    \
-    		  0,                            /* Dummy tx value for rx only funcs */    \
+              0,                            /* Dummy tx value for rx only funcs */    \
               spidrvMaster,                 /* SPI mode                         */    \
               spidrvBitOrderMsbFirst,       /* Bit order on bus                 */    \
               spidrvClockMode1,             /* SPI clock/phase mode             */    \
@@ -340,34 +340,34 @@ void SPI_Init() {
  ******************************************************************************/
 void UART_Init() {
 
-  CMU_ClockEnable(cmuClock_HFPER, true);
-  CMU_ClockEnable(cmuClock_USART0, true); // Enable clock for USART0 module
-  CMU_ClockEnable(cmuClock_GPIO, true);
+    CMU_ClockEnable(cmuClock_HFPER, true);
+    CMU_ClockEnable(cmuClock_USART0, true); // Enable clock for USART0 module
+    CMU_ClockEnable(cmuClock_GPIO, true);
 
-  GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 1); // TX
-  GPIO_PinModeSet(gpioPortC, 1, gpioModeInput, 0); // RX
+    GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 1); // TX
+    GPIO_PinModeSet(gpioPortC, 1, gpioModeInput, 0); // RX
 
-  USART_InitAsync_TypeDef usartInitUSART0 = {
-    .enable = usartDisable, 					// Initially disabled
-    .refFreq = 0,								// configured reference frequency
-    .baudrate = 115200, 				        // Baud rate defined in common.h
-    .oversampling = usartOVS16, 				// overSampling rate x16
-    .databits = USART_FRAME_DATABITS_EIGHT, 	// 8-bit frames
-    .parity = USART_FRAME_PARITY_NONE,			// parity - none
-    .stopbits = USART_FRAME_STOPBITS_ONE,		// 1 stop bit
-  };
+    USART_InitAsync_TypeDef usartInitUSART0 = {
+        .enable = usartDisable,                     // Initially disabled
+        .refFreq = 0,                               // configured reference frequency
+        .baudrate = 115200,                         // Baud rate defined in common.h
+        .oversampling = usartOVS16,                 // overSampling rate x16
+        .databits = USART_FRAME_DATABITS_EIGHT,     // 8-bit frames
+        .parity = USART_FRAME_PARITY_NONE,          // parity - none
+        .stopbits = USART_FRAME_STOPBITS_ONE,       // 1 stop bit
+    };
 
-  /*Initialize UART registers*/
-  USART_InitAsync(USART0, &usartInitUSART0);
+    /*Initialize UART registers*/
+    USART_InitAsync(USART0, &usartInitUSART0);
 
-  USART0 -> ROUTE = USART_ROUTE_RXPEN | USART_ROUTE_TXPEN | USART_ROUTE_LOCATION_LOC5;
+    USART0 -> ROUTE = USART_ROUTE_RXPEN | USART_ROUTE_TXPEN | USART_ROUTE_LOCATION_LOC5;
 
-  /* Inform NVIC of IRQ changes*/
-  NVIC_ClearPendingIRQ(USART0_TX_IRQn);
-  NVIC_EnableIRQ(USART0_TX_IRQn);
+    /* Inform NVIC of IRQ changes*/
+    NVIC_ClearPendingIRQ(USART0_TX_IRQn);
+    NVIC_EnableIRQ(USART0_TX_IRQn);
 
-  USART_Enable(USART0, usartEnable);
-  NVIC_SetPriority(USART0_TX_IRQn, 1);
+    USART_Enable(USART0, usartEnable);
+    NVIC_SetPriority(USART0_TX_IRQn, 1);
 }
 
 
